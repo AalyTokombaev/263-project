3805b510d31bbba02d2799743b6037a7842c5907
OlderNed <alexiver96@hotmail.com>
2023-05-05 10:43:25 +0200

    wiiiii more code!

--- pmerge.c
+++ pmerge.c
@@ -5,6 +5,8 @@
 
 void paraMerge(int *arr, int start, int mid, int end) {
 
+    // I'm just gonna assume they're sorted 
+
     // first we need to define the arrays A and B
     int size_A = mid - start + 1; // size of left array we want to merge;
     int size_B = end  - mid; // size of right array we want to merge;
@@ -20,27 +22,57 @@ void paraMerge(int *arr, int start, int mid, int end) {
         right[j] = arr[mid + 1  + j];
     }
 
-    // now we do the fun algorithm 
-
-    // we should divide the arrays into p parts 
-    // we should have p threads
-    // A = A_1, ..., A_p
-    // B = B_1, ..., B_p
+    // maybe we could do odd/even sorting here 
     
-    // ranges [la, ua] and [lb, ub]
-    int la = 0;
-    int lb = 0;
+    // Do I even need this?
+    // split A into odd indices and even indices
+    int middle = size_A / 2;
+    if (middle % 2 == 1) {
+        middle++;
+    }
+    // hey I coild do this in parallel!
+    int *A_even = malloc(sizeof(int*) * middle);
+    int *A_odd = malloc(sizeof(int*) * (size_A - middle));
 
-    int ua = size_A - 1;
-    int ub = size_B - 1;
 
-    // middles
-    int ma = (la + ua) / 2;
-    int mb = (lb + ub) / 2;
+    // for even a_i
+    int j = 0;
+    for (int i = 0; i < size_A; i++) {
+        if (i % 2 == 0) {
+            A_even[j] = left[i];
+            j++;
+        }
+    }
 
-    // compare middles
+    // for odd a_i
+    j = 0;
+    for (int i = 0; i < size_A; i++) {
+        if (i % 2 == 1) {
+            A_odd[j] = left[i];
+            j++;
+        }
+    }
 
-    
+    // i don't know if I need this
+    middle = size_B / 2;
+    if (middle % 2 == 1) {
+        middle++;
+    }
+
+    int *B_even = malloc(sizeof(int*) * middle);
+    int *B_odd = malloc(sizeof(int*) * (size_B - middle));
+
+    j = 0;
+    for (int i = 0; i < size_B; i++) {
+        if (i % 2 == 0) {
+            B_even[j] = right[i];
+            j++;
+        }
+    }
+
+
+
+
 
 
 
