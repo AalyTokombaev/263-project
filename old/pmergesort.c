d8bf8bbbb565dadf2f54136372745e133540b55b
OlderNed <alexiver96@hotmail.com>
2023-05-08 14:43:17 +0200

    same commit as above

--- old/pmergesort.c
+++ old/pmergesort.c
@@ -7,21 +7,6 @@
 
 void pmergeSort(int *arr, int start, int end) {
 
-    //  prints for debugging, I'll keep them here in case I'll need to debug again later.
-    /*
-    printf("in mergesort\n");
-    printf("start: %d, end: %d\n", start, end);
-    printf("part arr: ");
-    for (int i = start; i < end; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    printf("arr: ");
-    printArray(arr, 14);
-    printf("\n");
-    */
-
-
     if (start < end) {
         // find the middle and mergesort the left and right
         // int middle = start + (end - start) / 2;
